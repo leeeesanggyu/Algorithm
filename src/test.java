a8d56cfd09ad0fc4bc924c539fe58837cef93d98
salgu1998 <salgu1998@gmail.com>
2021-12-22 00:08:39 +0900

    fist commit

--- src/test.java
+++ src/test.java
@@ -1,2 +1,9 @@
 public class test {
+    public static void main(String[] args) {
+        System.out.println(solution(1));
+    }
+
+    public static int solution(int num) {
+        return 1;
+    }
 }
